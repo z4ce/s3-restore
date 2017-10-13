f0415fee38523aff22e1959cf5a02d25c824b43a
Ian Zink <izink@pivotal.io>
2017-10-13 13:37:15 -0500

    Add logging and fixes

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"time"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/araddon/dateparse"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
@@ -12,6 +14,7 @@ import (
 )
 
 func setVersion(svc *s3.S3, bucket string, key string, versionId string) error {
+	log.Info("Setting version: ", bucket, " ", key, " ", versionId)
 	if versionId == "DELETE" {
 		input := &s3.DeleteObjectInput{
 			Bucket: &bucket,
@@ -22,7 +25,7 @@ func setVersion(svc *s3.S3, bucket string, key string, versionId string) error {
 			return err
 		}
 	} else {
-		src := key + "?versionId=" + versionId
+		src := "/" + bucket + "/" + key + "?versionId=" + versionId
 		input := &s3.CopyObjectInput{
 			CopySource: &src,
 			Bucket:     &bucket,
@@ -38,18 +41,35 @@ func setVersion(svc *s3.S3, bucket string, key string, versionId string) error {
 }
 
 func processVersion(final *map[string]s3.ObjectVersion, target time.Time, obj *s3.ObjectVersion) {
-	if obj.LastModified.After(target) {
-		return
-	}
+	log.Print("Processing: ", obj)
+
 	key := *obj.Key
 	curObj, exists := (*final)[key]
+	log.Info("Key ", key, " exists ", exists)
 
 	if !exists {
+		log.Info("Adding key that doesn't exist", key)
+		// insert a delete marker for things that didn't exist
+		if obj.LastModified.After(target) {
+			fakeVersionID := "DELETE"
+			deleteObj := s3.ObjectVersion{
+				Key:          obj.Key,
+				LastModified: obj.LastModified,
+				VersionId:    &fakeVersionID,
+			}
+			(*final)[key] = deleteObj
+			return
+		}
 		(*final)[key] = *obj
 		return
 	}
-
-	if curObj.LastModified.After(*(*final)[key].LastModified) {
+	if obj.LastModified.After(target) {
+		return
+	}
+	// This is our object if
+	// If it is newer than what is currently in place
+	// b) curObj is currently a future delete marker
+	if curObj.LastModified.After(*(*final)[key].LastModified) || curObj.LastModified.After(target) {
 		(*final)[key] = *obj
 	}
 }
@@ -78,6 +98,8 @@ func processDeleteMarker(final *map[string]s3.ObjectVersion, target time.Time, o
 }
 
 func buildVersionDictionary(svc *s3.S3, bucket string, target time.Time) (final map[string]s3.ObjectVersion, err error) {
+	final = make(map[string]s3.ObjectVersion)
+	log.Print("Getting versions...")
 	err = svc.ListObjectVersionsPages(&s3.ListObjectVersionsInput{
 		Bucket: &bucket,
 	}, func(p *s3.ListObjectVersionsOutput, last bool) (shouldContinue bool) {
@@ -97,6 +119,7 @@ func buildVersionDictionary(svc *s3.S3, bucket string, target time.Time) (final 
 }
 
 func processDictionary(svc *s3.S3, bucket string, final map[string]s3.ObjectVersion) error {
+	log.Info("Processing final map", final)
 	for key, value := range final {
 		err := setVersion(svc, bucket, key, *value.VersionId)
 		if err != nil {
@@ -107,19 +130,33 @@ func processDictionary(svc *s3.S3, bucket string, final map[string]s3.ObjectVers
 }
 
 func process(c *cli.Context) error {
+	fmt.Println("Beginning processing")
 	sess := session.Must(session.NewSession())
 	svc := s3.New(sess)
 	targetTime, err := dateparse.ParseAny(c.GlobalString("time"))
+	log.Print("Parsed time ", err)
 	if err != nil {
-		return fmt.Errorf("Failed parsing time string: " + c.GlobalString("taget") + ":" + err.Error())
+		fmt.Println("Invalid time string", err)
+		return err
 	}
 	objects, err := buildVersionDictionary(svc, c.GlobalString("bucket"), targetTime)
 	if err != nil {
-		return fmt.Errorf("Failed building dictionary of versions" + err.Error())
+		fmt.Println("Failed building dictionary", err)
+		return err
 	}
-
+	fmt.Println("Found ", len(objects), " keys")
 	err = processDictionary(svc, c.GlobalString("bucket"), objects)
+	if err != nil {
+		fmt.Println("Failed processing dictionary", err)
+	}
+	fmt.Println("Done")
 	return err
+}
+func init() {
+	log.SetOutput(os.Stderr)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.ErrorLevel)
 }
 
 func main() {
@@ -133,14 +170,28 @@ func main() {
 			Name:  "time",
 			Usage: "time to restore to. Use format: 2006-01-02T15:04:05.999999999Z07:00",
 		},
+		cli.BoolFlag{
+			Name:  "debug",
+			Usage: "Use to enable debug logging",
+		},
 	}
 
 	app.Commands = []cli.Command{
+
 		{
 			Name:   "restore",
 			Usage:  "restore the s3 bucket to the time specified",
 			Action: process,
 		},
 	}
+	log.Print("Running App..")
+
+	app.Before = func(c *cli.Context) error {
+		if c.Bool("debug") {
+			log.SetLevel(log.DebugLevel)
+		}
+		return nil
+	}
+
 	app.Run(os.Args)
 }
